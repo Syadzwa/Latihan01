d0b67375c8859e121389770d380fc154e8e5151a
Syadzwa <syadzwaramadhan@gmail.com>
2025-11-21 19:16:26 +0700

    Menambahkan file page1, page2, page3.

---
+++ pages/page1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Welcome to the syadzwa's Home Page")
+st.write("Ini demo aplikasi Streamlit.")
---
+++ pages/page3.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("settings")
+st.write("This page allows you to customize your preferences")
